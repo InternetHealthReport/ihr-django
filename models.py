68c1da16e457933f924d92b74c2f24258afe412f
romain.fontugne <romain.fontugne@gmail.com>
2016-12-05 18:50:10 +0900

    added alarms table

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ class ASN(models.Model):
     def __str__(self):
         return "ASN%s %s" % (self.number, self.name)
 
+
 class Congestion(models.Model):
     timebin = models.DateTimeField(db_index=True)
     asn = models.ForeignKey(ASN, on_delete=models.CASCADE)
@@ -17,6 +18,33 @@ class Congestion(models.Model):
 
     def __str__(self):
         return "%s AS%s" % (self.timebin, self.asn.number)
+
+
+class Congestion_alarms(models.Model):
+    asn = models.ForeignKey(ASN, on_delete=models.CASCADE, db_index=True)
+    timebin = models.DateTimeField(db_index=True)
+    ip = models.CharField(max_length=64, db_index=True)
+    link = models.CharField(max_length=128, db_index=True)
+    medianrtt = models.FloatField(default=0.0)
+    diffMedian = models.FloatField(default=0.0)
+    deviation = models.FloatField(default=0.0)
+    nbProbes = models.IntergerField(default=0)
+
+    def __str__(self):
+        return "%s AS%s" % (self.timebin, self.asn.number)
+
+    
+class Forwarding_alarms(models.Model):
+    asn = models.ForeignKey(ASN, on_delete=models.CASCADE, db_index=True)
+    timebin = models.DateTimeField(db_index=True)
+    ip = models.CharField(max_length=64, db_index=True)
+    correlation = models.FloatField(default=0.0)
+    nbSamples = models.IntegerField(default=0)
+    refhops   = models.CharField(max_length=1024)
+    obshops   = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return "%s AS%s %s" % (self.timebin, self.asn.number, self.ip)
     
 
 class Forwarding(models.Model):
