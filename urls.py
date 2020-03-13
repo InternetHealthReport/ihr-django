03f634bcd2821dc6800b624f2249ee4a11a638d2
Romain Fontugne <romain.fontugne@gmail.com>
2020-03-13 05:06:45 +0000

    update api urls

--- urls.py
+++ urls.py
@@ -7,25 +7,46 @@ from drf_yasg import openapi
 from . import views
 from .user_api import urls as user_urls
 
+#from rest_framework_swagger.views import get_swagger_view
+#schema_view = get_swagger_view(title='API')
+from drf_yasg.generators import OpenAPISchemaGenerator
+
+
+exposed_api = [ 
+    url(r'^api/network/$', views.NetworkView.as_view(), name='networkListView'),
+    url(r'^api/delay/$', views.DelayView.as_view(), name='delayListView'),
+    url(r'^api/forwarding/$', views.ForwardingView.as_view(), name='forwardingListView'),
+    url(r'^api/hegemony/$', views.HegemonyView.as_view(), name='hegemonyListView'),
+    url(r'^api/delay/alarms/$', views.DelayAlarmsView.as_view(), name='delayAlarmsListView'),
+    url(r'^api/forwarding/alarms/$', views.ForwardingAlarmsView.as_view(), name='forwardingAlarmsListView'),
+    url(r'^api/disco/events/$', views.DiscoEventsView.as_view(), name='discoEventsListView'),
+    url(r'^api/disco/probes/$', views.DiscoProbesView.as_view(), name='discoProbesListView'),
+    url(r'^api/hegemony/$', views.HegemonyView.as_view(), name='hegemonyListView'),
+    url(r'^api/hegemony/alarms/$', views.HegemonyAlarmsView.as_view(), name='hegemonyAlarmsListView'),
+    url(r'^api/hegemony/cone/$', views.HegemonyConeView.as_view(), name='hegemonyConeListView'),
+    url(r'^api/network_delay/$', views.NetworkDelayView.as_view(), name='networkDelayListView'),
+    url(r'^api/network_delay/locations/$', views.NetworkDelayLocationsView.as_view(), name='networkDelayLocationsListView'),
+    url(r'^api/network_delay/alarms/$', views.NetworkDelayAlarmsView.as_view(), name='networkDelayAlarmsListView'),
+]   
+
 schema_view = get_schema_view(
        openapi.Info(
          title="IHR API",
-         default_version='v1',
+         default_version='',
          description="Data computed by Internet Health Report",
          terms_of_service="https://ihr.iijlab.net/ihr/en-us/documentation#Data_policy",
          contact=openapi.Contact(email="ihr-admin@iij-ii.co.jp"),
          license=openapi.License(name="Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)",
-             url="https://creativecommons.org/licenses/by-nc-sa/4.0/"),
+         url="https://creativecommons.org/licenses/by-nc-sa/4.0/"),
       ),
       public=True,
       permission_classes=(permissions.AllowAny,),
+      patterns=exposed_api,
  )
 
 app_name = 'ihr'
 urlpatterns = [
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     url(r'^$', views.index, name='index'),
     # url(r'^disco/$', views.index_disco, name='disco'),
     url(r'^search/$', views.search, name='search'),
@@ -40,19 +61,14 @@ urlpatterns = [
     url(r'^data/geodisco/$', views.discoGeoData, name='discoGeoData'),
     url(r'^data/hegemony/$', views.hegemonyData, name='hegemonyData'),
     url(r'^data/hegemonyCone/$', views.coneData, name='coneData'),
-    url(r'^api/$', views.restful_API, name="root"),
-    url(r'^api/network/$', views.NetworkView.as_view(), name='networkListView'),
-    url(r'^api/delay/$', views.DelayView.as_view(), name='delayListView'),
-    url(r'^api/forwarding/$', views.ForwardingView.as_view(), name='forwardingListView'),
+    #url(r'^api/$', views.restful_API, name="root"),
     url(r'^api/delay_alarms/$', views.DelayAlarmsView.as_view(), name='delayAlarmsListView'),
     url(r'^api/forwarding_alarms/$', views.ForwardingAlarmsView.as_view(), name='forwardingAlarmsListView'),
     url(r'^api/disco_events/$', views.DiscoEventsView.as_view(), name='discoEventsListView'),
     url(r'^api/disco_probes/$', views.DiscoProbesView.as_view(), name='discoProbesListView'),
-    url(r'^api/hegemony/$', views.HegemonyView.as_view(), name='hegemonyListView'),
     url(r'^api/hegemony_alarms/$', views.HegemonyAlarmsView.as_view(), name='hegemonyAlarmsListView'),
     url(r'^api/hegemony_cone/$', views.HegemonyConeView.as_view(), name='hegemonyConeListView'),
-    url(r'^api/network_delay/$', views.NetworkDelayView.as_view(), name='networkDelayListView'),
     url(r'^api/network_delay_locations/$', views.NetworkDelayLocationsView.as_view(), name='networkDelayLocationsListView'),
     url(r'^api/network_delay_alarms/$', views.NetworkDelayAlarmsView.as_view(), name='networkDelayAlarmsListView'),
     *user_urls.urlpatterns
-]
+] + exposed_api
