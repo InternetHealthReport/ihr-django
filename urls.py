5dd589c9f69120701b139aba70d594d14bde8af7
Romain Fontugne <romain.fontugne@gmail.com>
2020-03-29 14:48:54 +0000

    update urls for new website

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
 
 from . import views
 from .user_api import urls as user_urls
@@ -13,18 +15,18 @@ from drf_yasg.generators import OpenAPISchemaGenerator
 
 
 exposed_api = [ 
-    url(r'^api/networks/$', views.NetworkView.as_view(), name='networkListView'),
-    url(r'^api/link/delay/$', views.DelayView.as_view(), name='delayListView'),
-    url(r'^api/link/forwarding/$', views.ForwardingView.as_view(), name='forwardingListView'),
-    url(r'^api/link/delay/alarms/$', views.DelayAlarmsView.as_view(), name='delayAlarmsListView'),
-    url(r'^api/link/forwarding/alarms/$', views.ForwardingAlarmsView.as_view(), name='forwardingAlarmsListView'),
-    url(r'^api/disco/events/$', views.DiscoEventsView.as_view(), name='discoEventsListView'),
-    url(r'^api/hegemony/$', views.HegemonyView.as_view(), name='hegemonyListView'),
-    url(r'^api/hegemony/alarms/$', views.HegemonyAlarmsView.as_view(), name='hegemonyAlarmsListView'),
-    url(r'^api/hegemony/cones/$', views.HegemonyConeView.as_view(), name='hegemonyConeListView'),
-    url(r'^api/network_delay/$', views.NetworkDelayView.as_view(), name='networkDelayListView'),
-    url(r'^api/network_delay/locations/$', views.NetworkDelayLocationsView.as_view(), name='networkDelayLocationsListView'),
-    url(r'^api/network_delay/alarms/$', views.NetworkDelayAlarmsView.as_view(), name='networkDelayAlarmsListView'),
+    url(r'^networks/$', views.NetworkView.as_view(), name='networkListView'),
+    url(r'^link/delay/$', views.DelayView.as_view(), name='delayListView'),
+    url(r'^link/forwarding/$', views.ForwardingView.as_view(), name='forwardingListView'),
+    url(r'^link/delay/alarms/$', views.DelayAlarmsView.as_view(), name='delayAlarmsListView'),
+    url(r'^link/forwarding/alarms/$', views.ForwardingAlarmsView.as_view(), name='forwardingAlarmsListView'),
+    url(r'^disco/events/$', views.DiscoEventsView.as_view(), name='discoEventsListView'),
+    url(r'^hegemony/$', views.HegemonyView.as_view(), name='hegemonyListView'),
+    url(r'^hegemony/alarms/$', views.HegemonyAlarmsView.as_view(), name='hegemonyAlarmsListView'),
+    url(r'^hegemony/cones/$', views.HegemonyConeView.as_view(), name='hegemonyConeListView'),
+    url(r'^network_delay/$', views.NetworkDelayView.as_view(), name='networkDelayListView'),
+    url(r'^network_delay/locations/$', views.NetworkDelayLocationsView.as_view(), name='networkDelayLocationsListView'),
+    url(r'^network_delay/alarms/$', views.NetworkDelayAlarmsView.as_view(), name='networkDelayAlarmsListView'),
 ]   
 
 schema_view = get_schema_view(
@@ -46,29 +48,30 @@ schema_view = get_schema_view(
 app_name = 'ihr'
 urlpatterns = [
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^$', views.index, name='index'),
+    #url(r'^$', views.index, name='index'),
+    #url(r'^$', TemplateView.as_view(template_name='index.html')),
     # url(r'^disco/$', views.index_disco, name='disco'),
-    url(r'^search/$', views.search, name='search'),
-    url(r'^(?P<pk>-?[0-9]+)/asn/$', views.ASNDetail.as_view(), name='asnDetail'),
-    url(r'^(?P<pk>[A-Z]+)/country/$', views.CountryDetail.as_view(), name='countryDetail'),
-    url(r'^(?P<pk>[0-9]+)/disco/$', views.DiscoDetail.as_view(), name='discoDetail'),
-    url(r'^monitoredAsn/$', views.ASNList.as_view(), name='asnList'),
-    url(r'^monitoredCountry/$', views.CountryList.as_view(), name='countryList'),
+    #url(r'^search/$', views.search, name='search'),
+    #url(r'^(?P<pk>-?[0-9]+)/asn/$', views.ASNDetail.as_view(), name='asnDetail'),
+    #url(r'^(?P<pk>[A-Z]+)/country/$', views.CountryDetail.as_view(), name='countryDetail'),
+    #url(r'^(?P<pk>[0-9]+)/disco/$', views.DiscoDetail.as_view(), name='discoDetail'),
+    #url(r'^monitoredAsn/$', views.ASNList.as_view(), name='asnList'),
+    #url(r'^monitoredCountry/$', views.CountryList.as_view(), name='countryList'),
     # TODO remove /data/ endpoints
-    url(r'^data/delay/$', views.delayData, name='delayData'),
-    url(r'^data/forwarding/$', views.forwardingData, name='forwardingData'),
-    url(r'^data/disco/$', views.discoData, name='discoData'),
-    url(r'^data/geodisco/$', views.discoGeoData, name='discoGeoData'),
-    url(r'^data/hegemony/$', views.hegemonyData, name='hegemonyData'),
-    url(r'^data/hegemonyCone/$', views.coneData, name='coneData'),
+    #url(r'^data/delay/$', views.delayData, name='delayData'),
+    #url(r'^data/forwarding/$', views.forwardingData, name='forwardingData'),
+    #url(r'^data/disco/$', views.discoData, name='discoData'),
+    #url(r'^data/geodisco/$', views.discoGeoData, name='discoGeoData'),
+    #url(r'^data/hegemony/$', views.hegemonyData, name='hegemonyData'),
+    #url(r'^data/hegemonyCone/$', views.coneData, name='coneData'),
     #url(r'^api/$', views.restful_API, name="root"),
-    url(r'^api/delay_alarms/$', views.DelayAlarmsView.as_view(), name='delayAlarmsListView'),
-    url(r'^api/forwarding_alarms/$', views.ForwardingAlarmsView.as_view(), name='forwardingAlarmsListView'),
-    url(r'^api/disco_events/$', views.DiscoEventsView.as_view(), name='discoEventsListView'),
-    url(r'^api/disco_probes/$', views.DiscoProbesView.as_view(), name='discoProbesListView'),
-    url(r'^api/hegemony_alarms/$', views.HegemonyAlarmsView.as_view(), name='hegemonyAlarmsListView'),
-    url(r'^api/hegemony_cone/$', views.HegemonyConeView.as_view(), name='hegemonyConeListView'),
-    url(r'^api/network_delay_locations/$', views.NetworkDelayLocationsView.as_view(), name='networkDelayLocationsListView'),
-    url(r'^api/network_delay_alarms/$', views.NetworkDelayAlarmsView.as_view(), name='networkDelayAlarmsListView'),
+    url(r'^delay_alarms/$', views.DelayAlarmsView.as_view(), name='delayAlarmsListView'),
+    url(r'^forwarding_alarms/$', views.ForwardingAlarmsView.as_view(), name='forwardingAlarmsListView'),
+    url(r'^disco_events/$', views.DiscoEventsView.as_view(), name='discoEventsListView'),
+    url(r'^disco_probes/$', views.DiscoProbesView.as_view(), name='discoProbesListView'),
+    url(r'^hegemony_alarms/$', views.HegemonyAlarmsView.as_view(), name='hegemonyAlarmsListView'),
+    url(r'^hegemony_cone/$', views.HegemonyConeView.as_view(), name='hegemonyConeListView'),
+    url(r'^network_delay_locations/$', views.NetworkDelayLocationsView.as_view(), name='networkDelayLocationsListView'),
+    url(r'^network_delay_alarms/$', views.NetworkDelayAlarmsView.as_view(), name='networkDelayAlarmsListView'),
     *user_urls.urlpatterns
 ] + exposed_api
